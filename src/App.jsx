98f69be255eda2ea6ea8f20c6e968880363ff874
신희성 <vvwv3092@gmail.com>
2024-01-21 16:17:42 +0900

    Add mock data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const MOCK_DATA = [
+  {
+    id: "1234-asdf-asdf-asdf",
+    start_date: "2023-01-01 10:10:10",
+    expiration_date: "2023-01-01 10:10:10",
+    cpu: "2v",
+    memory: 2,
+    disk: 30,
+    bandwidth: 10,
+    remote_access_address: "raplegend.cloud:8080 -> 8080",
+    service_address: "raplegend.cloud:8080 -> 8080",
+    username: "신희성",
+    default_password: "springboot123",
+    price: 510000000,
+  },
+  {
+    id: "1234-qwer-asdf-asdf",
+    start_date: "2023-01-01 10:10:10",
+    expiration_date: "2023-01-01 10:10:10",
+    cpu: "2v",
+    memory: 2,
+    disk: 30,
+    bandwidth: 10,
+    remote_access_address: "raplegend.cloud:8080 -> 8080",
+    service_address: "raplegend.cloud:8080 -> 8080",
+    username: "신희성",
+    default_password: "springboot123",
+    price: 510000000,
+  },
+  {
+    id: "1234-hjkl-asdf-asdf",
+    start_date: "2023-01-01 10:10:10",
+    expiration_date: "2023-01-01 10:10:10",
+    cpu: "2v",
+    memory: 2,
+    disk: 30,
+    bandwidth: 10,
+    remote_access_address: "raplegend.cloud:8080 -> 8080",
+    service_address: "raplegend.cloud:8080 -> 8080",
+    username: "신희성",
+    default_password: "springboot123",
+    price: 510000000,
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
